029274dc9eef6b494d31c354c6401d23a5401c14
gamingcoil <84976465+gamingcoil@users.noreply.github.com>
2023-06-30 13:43:28 +0300

    create server

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongodb = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI;
+
+let db;
+
+mongodb.MongoClient.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(client => {
+    db = client.db();
+    console.log('Connected to MongoDB');
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB:', err);
+  });
+
+app.use(express.static('public'));
+
+app.get('/items', (req, res) => {
+  db.collection('items')
+    .find({})
+    .project({ _id: 0, id: 1, title: 1, imageLink: 1 })
+    .toArray()
+    .then(items => {
+      res.json(items);
+    })
+    .catch(err => {
+      console.error('Failed to fetch items:', err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+app.get('/items/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+  db.collection('items')
+    .findOne({ id: itemId })
+    .then(item => {
+      if (!item) {
+        res.status(404).send('Item not found');
+      } else {
+        res.json(item);
+      }
+    })
+    .catch(err => {
+      console.error('Failed to fetch item:', err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+app.get('/items/all', (req, res) => {
+  db.collection('items')
+    .find({})
+    .toArray()
+    .then(items => {
+      res.json(items);
+    })
+    .catch(err => {
+      console.error('Failed to fetch all items:', err);
+      res.status(500).send('Internal Server Error');
+    });
+});
